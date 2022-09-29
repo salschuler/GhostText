8d2ed189b87607bb4628ba4e19491d896bd215d0
Schuyler Alschuler <salschuler@amplify.com>
2022-09-29 16:46:39 -0700

    formatting

--- source/background.js
+++ source/background.js
@@ -1,23 +1,26 @@
-import browser from 'webextension-polyfill';
-import oneEvent from 'one-event';
-import optionsStorage from './options-storage.js';
+import browser from "webextension-polyfill";
+import oneEvent from "one-event";
+import optionsStorage from "./options-storage.js";
 
 function inCurrentTab(callback) {
-	chrome.tabs.query({
-		active: true,
-		currentWindow: true,
-	}, tabs => callback(tabs[0]));
+	chrome.tabs.query(
+		{
+			active: true,
+			currentWindow: true,
+		},
+		(tabs) => callback(tabs[0])
+	);
 }
 
 function handleClose(info, tab) {
 	chrome.tabs.executeScript(tab.id, {
-		code: 'stopGT()',
+		code: "stopGT()",
 	});
 }
 
-async function handleAction({id}) {
+async function handleAction({ id }) {
 	const defaults = {
-		runAt: 'document_start',
+		runAt: "document_start",
 		allFrames: true,
 	};
 	const [alreadyInjected] = await browser.tabs.executeScript(id, {
@@ -26,79 +29,86 @@ async function handleAction({id}) {
 	});
 	console.log(alreadyInjected);
 	if (alreadyInjected) {
-		return browser.tabs.executeScript(id, {...defaults, code: 'startGT()'});
+		return browser.tabs.executeScript(id, { ...defaults, code: "startGT()" });
 	}
 
 	try {
 		await Promise.all([
-			browser.tabs.insertCSS(id, {...defaults, file: '/ghost-text.css'}),
-			browser.tabs.executeScript(id, {...defaults, file: '/ghost-text.js'}),
+			browser.tabs.insertCSS(id, { ...defaults, file: "/ghost-text.css" }),
+			browser.tabs.executeScript(id, { ...defaults, file: "/ghost-text.js" }),
 		]);
 	} catch (error) {
 		console.error(error);
 	}
 
-	await browser.tabs.executeScript(id, {...defaults, code: 'startGT()'});
+	await browser.tabs.executeScript(id, { ...defaults, code: "startGT()" });
 }
 
 function handlePortListenerErrors(listener) {
-	return async port => {
+	return async (port) => {
 		try {
 			await listener(port);
 		} catch (error) {
-			let {message} = error;
-			if ([
-				'Failed to fetch',
-				'NetworkError when attempting to fetch resource.',
-				'Could not connect to the server.',
-			].includes(message)) {
-				message = 'Unable to connect to the editor. <a href="https://ghosttext.fregante.com/troubleshooting/#unable-to-connect">Need help?</a>';
+			let { message } = error;
+			if (
+				[
+					"Failed to fetch",
+					"NetworkError when attempting to fetch resource.",
+					"Could not connect to the server.",
+				].includes(message)
+			) {
+				message =
+					'Unable to connect to the editor. <a href="https://ghosttext.fregante.com/troubleshooting/#unable-to-connect">Need help?</a>';
 			}
 
-			port.postMessage({error: message});
+			port.postMessage({ error: message });
 		}
 	};
 }
 
-chrome.runtime.onConnect.addListener(handlePortListenerErrors(async port => {
-	console.assert(port.name === 'new-field');
-	const {serverPort} = await optionsStorage.getAll();
-	const response = await fetch(`http://localhost:${serverPort}`);
-	const {ProtocolVersion, WebSocketPort} = await response.json();
-	if (ProtocolVersion !== 1) {
-		throw new Error('Incompatible protocol version');
-	}
+chrome.runtime.onConnect.addListener(
+	handlePortListenerErrors(async (port) => {
+		console.assert(port.name === "new-field");
+		const { serverPort } = await optionsStorage.getAll();
+		const response = await fetch(`http://localhost:${serverPort}`);
+		const { ProtocolVersion, WebSocketPort } = await response.json();
+		if (ProtocolVersion !== 1) {
+			throw new Error("Incompatible protocol version");
+		}
 
-	console.log('will open socket');
-	const socket = new WebSocket('ws://localhost:' + WebSocketPort);
-	const event = await Promise.race([
-		oneEvent(socket, 'open'),
-		oneEvent(socket, 'error'),
-	]);
-	console.log(event);
+		console.log("will open socket");
+		const socket = new WebSocket("ws://localhost:" + WebSocketPort);
+		const event = await Promise.race([
+			oneEvent(socket, "open"),
+			oneEvent(socket, "error"),
+		]);
+		console.log(event);
 
-	const onSocketClose = () => port.postMessage({close: true});
-	socket.addEventListener('close', onSocketClose);
-	socket.addEventListener('message', event => port.postMessage({message: event.data}));
-	socket.addEventListener('error', event => console.error('error!', event));
+		const onSocketClose = () => port.postMessage({ close: true });
+		socket.addEventListener("close", onSocketClose);
+		socket.addEventListener("message", (event) =>
+			port.postMessage({ message: event.data })
+		);
+		socket.addEventListener("error", (event) => console.error("error!", event));
 
-	port.onMessage.addListener(message => {
-		console.log('got message from script', message);
-		socket.send(message);
-	});
-	console.log(port);
-	port.onDisconnect.addListener(() => {
-		socket.removeEventListener('close', onSocketClose);
-		socket.close();
-	});
-	port.postMessage({ready: true});
-}));
+		port.onMessage.addListener((message) => {
+			console.log("got message from script", message);
+			socket.send(message);
+		});
+		console.log(port);
+		port.onDisconnect.addListener(() => {
+			socket.removeEventListener("close", onSocketClose);
+			socket.close();
+		});
+		port.postMessage({ ready: true });
+	})
+);
 
-function handleMessages({code, count}, {tab}) {
-	if (code === 'connection-count') {
-		let text = '';
+function handleMessages({ code, count }, { tab }) {
+	if (code === "connection-count") {
+		let text = "";
 		if (count === 1) {
-			text = '\u2713'; // ✓ checkmark. Safari encoding issue https://developer.apple.com/forums/thread/660798
+			text = "\u2713"; // ✓ checkmark. Safari encoding issue https://developer.apple.com/forums/thread/660798
 		} else if (count > 1) {
 			text = String(count);
 		}
@@ -107,9 +117,9 @@ function handleMessages({code, count}, {tab}) {
 			text,
 			tabId: tab.id,
 		});
-	} else if (code === 'focus-tab') {
-		chrome.tabs.update(tab.id, {active: true});
-		chrome.windows.update(tab.windowId, {focused: true});
+	} else if (code === "focus-tab") {
+		chrome.tabs.update(tab.id, { active: true });
+		chrome.windows.update(tab.windowId, { focused: true });
 	}
 }
 
@@ -117,38 +127,38 @@ function init() {
 	chrome.browserAction.onClicked.addListener(handleAction);
 	chrome.runtime.onMessage.addListener(handleMessages);
 	chrome.contextMenus.create({
-		id: 'stop-gt',
-		title: 'Disconnect GhostText on this page',
-		contexts: ['browser_action'],
+		id: "stop-gt",
+		title: "Disconnect GhostText on this page",
+		contexts: ["browser_action"],
 		onclick: handleClose,
 	});
-	chrome.commands.onCommand.addListener(command => {
-		if (command === 'open') {
+	chrome.commands.onCommand.addListener((command) => {
+		if (command === "open") {
 			inCurrentTab(handleAction);
 		}
 	});
 
 	chrome.contextMenus.create({
-		id: 'start-gt-editable',
-		title: 'Activate GhostText on field',
-		contexts: ['editable'],
+		id: "start-gt-editable",
+		title: "Activate GhostText on field",
+		contexts: ["editable"],
 		onclick: handleAction,
 	});
 
 	chrome.browserAction.setBadgeBackgroundColor({
-		color: '#008040',
+		color: "#008040",
 	});
 
-	browser.runtime.onInstalled.addListener(async ({reason}) => {
+	browser.runtime.onInstalled.addListener(async ({ reason }) => {
 		// Only notify on install
-		if (reason === 'install') {
-			const {installType} = await browser.management.getSelf();
-			if (installType === 'development') {
+		if (reason === "install") {
+			const { installType } = await browser.management.getSelf();
+			if (installType === "development") {
 				return;
 			}
 
 			await browser.tabs.create({
-				url: 'https://ghosttext.fregante.com/welcome/',
+				url: "https://ghosttext.fregante.com/welcome/",
 				active: true,
 			});
 		}
